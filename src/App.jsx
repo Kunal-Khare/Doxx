1f30070188d219eb20df5c80ef56f9903db0eb1f
Kunal-Khare <kunalkhare47@gmail.com>
2025-06-17 02:06:08 +0530

    some-Bg

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Bg from "./components/Bg";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-800">
+      <Bg />
+
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Forground.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+function Forground() {
+  return (
+    <div className="fixed z-[3] w-full h-full">
+      <div className="w-60 h-72 rounded-md bg-amber-300"></div>
+    </div>
+  );
+}
